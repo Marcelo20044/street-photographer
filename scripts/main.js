182962c7e6aaf49c7d18629b6a05c3fc0b959820
marcelo <khmarcelo044@gmail.com>
2024-10-24 23:00:15 +0300

    lab 4 done

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+function loadTime() {
+    const [navigation] = performance.getEntriesByType('navigation');
+    const loadTime = navigation.domContentLoadedEventEnd - navigation.startTime;
+
+    const footer = document.querySelector('footer')
+    const p = document.createElement('p')
+    p.textContent = `Скорость загрузки страницы: ${Math.round(loadTime)} мс.`
+    footer.appendChild(p)
+}
+
+function updateNavActive() {
+    let activeExists = false
+    const menuItems = document.querySelectorAll('.menu-item')
+
+    menuItems.forEach(item => {
+        if (item.href === document.location.href) {
+            item.classList.add('active')
+            activeExists = true
+        }
+    });
+
+    if (!activeExists) {
+        document.querySelector('.main-menu-item').classList.add('active');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(() => {
+        loadTime()
+    })
+    updateNavActive()
+});
